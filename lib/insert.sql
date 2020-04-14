679db0817e102b24d7e0f75f0a3d71e890cd1f73
jason-nordheim <jason.nordheim@gmail.com>
2020-04-14 13:32:37 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,136 @@
+-- Mr. Chocolate
+INSERT INTO bears (
+  [name], 
+  [age], 
+  [gender],
+  [color],
+  [temperament], 
+  [alive] 
+  ) VALUES (
+  "Mr. Chocolate", -- 1
+  20, -- 2
+  "M", -- 3 
+  "dark brown", -- 4
+  "calm", -- 5 
+  1 -- 6
+); 
+
+-- Rowdy 
+INSERT INTO bears (
+  [name], 
+  [age], 
+  [gender],
+  [color],
+  [temperament], 
+  [alive] 
+  ) VALUES (
+  "Rowdy", -- 1
+  10, -- 2 
+  "M", -- 3
+  "black", -- 4
+  "energetic", -- 5 
+  1 -- 6
+); 
+
+-- Grinch 
+INSERT INTO bears (
+  [name], 
+  [age], 
+  [gender],
+  [color],
+  [temperament], 
+  [alive] 
+  ) VALUES (
+  "Grinch", -- 1
+  2, -- 2
+  "M", -- 3
+  "brown", -- 4
+  "aggressvie", -- 5
+  1 -- 6 
+  ); 
+
+-- Tabitha 
+INSERT INTO bears (
+  [name], 
+  [age], 
+  [gender],
+  [color],
+  [temperament], 
+  [alive] 
+  ) VALUES (
+    "Tabitha", -- 1 
+    6, -- 2
+    "F", -- 3
+    "brown", -- 4
+    "calm", -- 5
+    1 -- 6 
+  ); 
+
+-- Melissa 
+INSERT INTO bears (
+  [name], 
+  [age], 
+  [gender],
+  [color],
+  [temperament], 
+  [alive] 
+  ) VALUES (
+  "Melissa", -- 1
+  13, -- 2 
+  "F", -- 3
+  "black", -- 4
+  "calm", -- 5
+  1 -- 6 
+); 
+
+-- Wendy 
+INSERT INTO bears (
+  [name], 
+  [age], 
+  [gender],
+  [color],
+  [temperament], 
+  [alive] 
+  ) VALUES (
+    "Wendy", -- 1 
+    6, -- 2
+    "F", -- 3
+    "black", -- 4
+    "calm", -- 5
+    1 -- 6 
+    ); 
+
+-- Sergeant Brown 
+INSERT INTO bears (
+  [name], 
+  [age], 
+  [gender],
+  [color],
+  [temperament], 
+  [alive] 
+  ) VALUES (
+  "Sergeant Brown",
+  19,
+  "M",
+  "brown",
+  "friendly",
+  1
+);
+
+-- no name 
+INSERT INTO bears (
+  [name], 
+  [age], 
+  [gender],
+  [color],
+  [temperament], 
+  [alive] 
+  ) VALUES (
+  null, -- 1 
+  20, -- 2
+  "M", -- 3
+  "Black", -- 4
+  "aggressive", -- 5
+  0 -- 6 
+); 
+ 
