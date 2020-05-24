025f65ad23172771e6a2b0b31ec9f1315bee48f0
LynnAmsbury <Lynn8542@gmail.com>
2020-05-24 11:41:44 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "M", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1 , "M", "brown", "active", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5 , "F", "black", "cautious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 7 , "M", "brown", "domineering", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3 , "F", "blonde", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 19 , "M", "green", "cranky", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 7 , "F", "black", "active", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 5 , "M", "black", "dead", 0);
+
+/* INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+                  ('Mr. Chocolate', '3', 'M', 'Brown', 'Sweet', '0'),
+                  ('Rowdy', '1', 'M', 'Brown', 'Active', '1'),
+                  ('Tabitha', '5', 'F', 'Black', 'Cautious', '1'),
+                  ('Sergeant Brown', '7', 'M', 'Brown', 'Domineering', '1'),
+                  ('Melissa', '3', 'F', 'Blonde', 'Sweet', '1'),
+                  ('Grinch', '19', 'M', 'Green', 'Cranky', '0'),
+                  ('Wendy', '2', 'F', 'Blonde', 'Flighty', '1'),
+                  (NULL, '11', 'M', 'White', 'Aggressive', '0') */
